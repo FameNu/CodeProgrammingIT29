a7ec816d68e249785f91843a07b08f57e51b00bb
FameNu <110712106+FameNu@users.noreply.github.com>
2023-08-03 11:56:11 +0700

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author USER
- */
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++){
